cfdbc4912e7faff008085f1aac3ed65483da1405
NunezGabriel <gabriel.nunez.arenas@gmail.com>
2024-11-22 09:09:42 -0500

    Agrengando clase abstracta de citas

--- src/clase_abstracta/Cita.java
+++ src/clase_abstracta/Cita.java
@@ -8,6 +8,50 @@ package clase_abstracta;
  *
  * @author emili
  */
-public class Cita {
+abstract class Cita {
+    private String fecha;
+    private String hora;
+    private String especialidad;
+    private String estado;
     
+    public Cita(String fecha, String hora, String especialidad, String estado){
+        this.fecha = fecha;
+        this.hora = hora;
+        this.especialidad = especialidad;
+        this.estado = estado;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
+    public abstract void registrarCita();
 }
